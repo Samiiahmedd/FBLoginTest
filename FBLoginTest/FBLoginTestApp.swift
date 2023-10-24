55222664be147957e8acc61930dca36ac7a102e1
Samiiahmed <eldon.smsm135@gmail.com>
2023-10-24 12:17:29 +0300

    Initial Commit

---
+++ FBLoginTest/FBLoginTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  FBLoginTestApp.swift
+//  FBLoginTest
+//
+//  Created by Sami Ahmed on 24/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct FBLoginTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
